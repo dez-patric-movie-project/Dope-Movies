e2af03e2375b6c3b36c7cfb5327bd258d9eddc81
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-10-15 18:06:51 -0400

    dope start

---
+++ dope-movies.js
@@ -0,0 +1,31 @@
+'use strict';
+console.log('hi');
+$( document ).ready(function() {
+    console.log( "ready!" );
+const dopeAPI = 'https://wiggly-sassy-impatiens.glitch.me/movies';
+
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+    $(window).on('load', function(){
+        setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+    });
+    function removeLoader(){
+        $( "#loadingDiv" ).fadeOut(500, function() {
+            // fadeOut complete. Remove the loading div
+            $( "#loadingDiv" ).remove(); //makes page more lightweight
+        });
+    }
+    function showMovies(list) {
+        $('#movieList').htmml('')
+
+    }
+    console.log(list);
+    function getMovies() {
+    return fetch(dopeAPI)
+        .then((response)=>response.json())
+}
+getMovies().then((movies)=>console.log(movies))
+
+
+
+
+});
