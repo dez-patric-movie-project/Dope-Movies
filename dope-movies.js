bf597c7be1ab48a0d3aeabbc2ca5e13cfb209945
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-10-18 16:49:16 -0400

    Codeup Jesus saved us

--- dope-movies.js
+++ dope-movies.js
@@ -1,91 +1,113 @@
 'use strict';
 
-$( document ).ready(function() {
-    const dopeAPI = 'https://wiggly-sassy-impatiens.glitch.me/movies';
+$(document).ready(function () {
+
 
     $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
     $(window).on('load', function () {
         setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
     });
 
-    function removeLoader() {
-        $("#loadingDiv").fadeOut(500, function () {
-            // fadeOut complete. Remove the loading div
-            $("#loadingDiv").remove(); //makes page more lightweight
-        });
-    }
-
-    function getMovies() {
-        return fetch(dopeAPI)
-            .then((response) => response.json())
-    }
 
     getMovies().then((movies) => {
         console.log(movies)
-        let movieCard = {
-            title: movies[0].title,
-            poster: movies[0].poster,
-            actors: movies[0].actors,
-            director: movies[0].director
-        }
         // console.log(movieCard);
         removeLoader()
         movies.forEach(movieDisplay)
+    })
 
-        function movieDisplay(movie) {
-            // console.log(movie)
+    $("#addMovie").click(function (e) {
+        e.preventDefault()
+        let movie = {
+            title: $('#userTitle').val(),
+            rating: $('#userRating').val()
+        }
+        addMovie(movie)
+    });
 
-            // for(var i = 0; i < movie.length; i++) {
+    $('#deleteMovie').click(function (e) {
+        e.preventDefault()
+        $(this).parent().remove(this);
+        deleteMovie(this)
+        console.log(this)
+    });
 
-                $(".movies").append(`<div class='card m-2 text-center card-width' id='${movie.id}'>
+
+});
+const dopeAPI = 'https://wiggly-sassy-impatiens.glitch.me/movies';
+const omdbApiKey = 'afe37df3'
+function movieDisplay(movie) {
+    console.log(movie)
+
+    // for(var i = 0; i < movie.length; i++) {
+
+    $(".movies").append(`<div class='card m-2 text-center card-width' id='${movie.id}'>
                                 <div class="card-body text-wrap">
                                 <h5>${movie.title}</h5>
                                 <img src="${movie.poster}"></img>
                                 <p class="card-text p-0 m-0 ">Rating: ${movie.rating}</p>
                                 <p class="card-text p-0 m-0 mb-2">Genre: ${movie.genre}</p>
-                                <button type="button" class="delete-btn btn btn-danger">Delete</button></div></div>`)
+                                <button type="button" id="deleteMovie" class="delete-btn btn btn-danger">Delete</button></div></div>`)
 
+}
 
-                $(".btn").click(function (e) {
-                    e.preventDefault()
-                    console.log(movies)
+function removeLoader() {
+    $("#loadingDiv").fadeOut(500, function () {
+        // fadeOut complete. Remove the loading div
+        $("#loadingDiv").remove(); //makes page more lightweight
+    });
+}
 
-                });
+function getMovies() {
+    return fetch(dopeAPI)
+        .then((response) => response.json())
+}
 
+function addMovie(movie) {
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movie) //Convert the JS object into a JSON string before sending it up to the server.
+    }
+    fetch(dopeAPI, options)
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonData) => {
+            console.log(jsonData);
+            $('#movies').html('')
+            getMovies().then((movies) => {
+                console.log(movies)
+                // console.log(movieCard);
+                removeLoader()
+                movies.forEach(movieDisplay)
+            })
+        })
+}
+function deleteMovie(movie) {
+    let options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movie)
+    }
+    fetch(dopeAPI,options)
+        .then((response)=>{
+            return response.json();
+        })
+        .then((jsonData)=>{
+            console.log(jsonData)
+            $('#movies').html('')
+            getMovies().then((movies) => {
+                console.log(movies)
+                // console.log(movieCard);
+                removeLoader()
+                movies.forEach(movieDisplay)
+            })
 
-            function addMovie(movie) {
-                let options = {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(movie) //Convert the JS object into a JSON string before sending it up to the server.
-                }
-                return fetch(dopeAPI, options)
-                    .then((response) => {
-                        console.log(response.json())
-                        console.log(addMovie())
-                        response.json()
-                    })
-            }
+        })
 
-
-        }
-    })
-
-
-});
-
-// var html = "<div class=\"row col card card-body text-center\" style=\" width: 18rem; margin: 1em;\">" +
-//     // '<div>' + reverseGeocode(data.lat.lon) + '</div>'+
-//     '<h6>Date: ' + convertDateTime(data.daily[i].dt) + '</h6>' +
-//     '<div>Today: ' + data.daily[i].temp.day + '</div>' +
-//     '<div>' +    '<img src="http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '.png">' + '</img>'  + '</div>' +
-//     '<div>L: ' + data.daily[i].temp.min + '</div>' +
-//     '<div>H: ' + data.daily[i].temp.max + '</div>' +
-//     '<div>Tonight: ' + data.daily[i].temp.night + '</div>' +
-//     '<div>Conditions: ' + data.daily[i].weather[0].description + '</div>' +
-//
-//     '</div>'
-// $('#five-day').append(html)
-// }
+}
